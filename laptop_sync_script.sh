81117e5c32574057cfbb99b492f73db52f36c194
Rsb23 <rsbarbu23@gmail.com>
2025-06-17 12:28:25 -0400

    fixed path by replacing ~ with /home/r34_runna

--- laptop_sync_script.sh
+++ laptop_sync_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-dotfiles_dir="~/Documents/dotfiles/"
+dotfiles_dir="$HOME/Documents/dotfiles/"
 
 git pull
 
-ln -s "$dotfiles_dir/.bashrc" "~/.bashrc"
-ln -s "$dotfiles_dir/.desktop_aliases" "~/.desktop_aliases"
-ln -s "$dotfiles_dir/.shared_aliases" "~/.shared_aliases"
-ln -s "$dotfiles_dir/kitty.conf" "~/.config/kitty/kitty.conf"
+ln -s "$dotfiles_dir/.bashrc" "$HOME/.bashrc"
+ln -s "$dotfiles_dir/.desktop_aliases" "$HOME/.desktop_aliases"
+ln -s "$dotfiles_dir/.shared_aliases" "$HOME/.shared_aliases"
+ln -s "$dotfiles_dir/kitty.conf" "$HOME/.config/kitty/kitty.conf"
