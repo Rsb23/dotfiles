39c60ecc70e8a9558363bd7501fac59c680883eb
Rsb23 <rsbarbu23@gmail.com>
2025-06-17 12:19:42 -0400

    changed aliases names, created syncing scripts

--- laptop_sync_script.sh
+++ laptop_sync_script.sh
@@ -1,1 +1,10 @@
 #!/bin/bash/
+
+dotfiles_dir="~/Documents/dotfiles/"
+
+git pull
+
+ln -s "$dotfiles_dir/.bashrc" "~/.bashrc"
+ln -s "$dotfiles_dir/.desktop_aliases" "~/.desktop_aliases"
+ln -s "$dotfiles_dir/.shared_aliases" "~/.shared_aliases"
+ln -s "$dotfiles_dir/kitty.conf" "~/.config/kitty/kitty.conf"
