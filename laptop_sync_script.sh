8102556bb28bed4ba1fd3f3aa784a26c4c1f9313
Rsb23 <rsbarbu23@gmail.com>
2025-06-17 12:30:59 -0400

    added forcing of symbolic link creation

--- laptop_sync_script.sh
+++ laptop_sync_script.sh
@@ -4,7 +4,7 @@ dotfiles_dir="$HOME/Documents/dotfiles/"
 
 git pull
 
-ln -s "$dotfiles_dir/.bashrc" "$HOME/.bashrc"
-ln -s "$dotfiles_dir/.desktop_aliases" "$HOME/.desktop_aliases"
-ln -s "$dotfiles_dir/.shared_aliases" "$HOME/.shared_aliases"
-ln -s "$dotfiles_dir/kitty.conf" "$HOME/.config/kitty/kitty.conf"
+ln -sf "$dotfiles_dir/.bashrc" "$HOME/.bashrc"
+ln -sf "$dotfiles_dir/.desktop_aliases" "$HOME/.desktop_aliases"
+ln -sf "$dotfiles_dir/.shared_aliases" "$HOME/.shared_aliases"
+ln -sf "$dotfiles_dir/kitty.conf" "$HOME/.config/kitty/kitty.conf"
