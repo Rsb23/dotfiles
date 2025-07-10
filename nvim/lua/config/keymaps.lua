1db440ff3eccf1d9a7c02e8560c27e22c03d137c
Rsb23 <rsbarbu23@gmail.com>
2025-07-10 17:45:58 -0400

    tab movement nvim keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,8 @@
+-- folder tree
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>e', function()
     vim.cmd("NvimTreeFocus")
 end)
+
+-- tab movement
+vim.api.nvim_set_keymap('t','<ESC>','<C-\\><C-n>gt',{noremap = true})  -- easier way to exit terminal mode
