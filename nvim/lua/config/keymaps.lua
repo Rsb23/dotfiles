1a6b7bbf6cefdadd7a3222910044637d430b77b8
Rsb23 <rsbarbu23@gmail.com>
2025-07-22 19:37:30 -0400

    removed focus, added terminal mappings to keymaps.lua, added neoscroll.lua for smooth scrolling

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,9 +1,18 @@
 -- folder tree
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ','
 vim.keymap.set('n', '<leader>e', function()
     vim.cmd("NvimTreeFocus")
 end)
 
+-- leader keys
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ','
+
 -- tab movement
-vim.api.nvim_set_keymap('t','<ESC>','<C-\\><C-n>gt',{noremap = true})  -- easier way to exit terminal mode
+vim.api.nvim_set_keymap('t','<ESC>','<C-W>C', {noremap = true})  -- easier way to exit terminal mode
+
+-- enable spellcheck (turned off by default)
+vim.o.spell = true
+vim.o.spelllang = { "en_us" }
+
+-- bottom terminal
+vim.api.nvim_set_keymay('n', '<Leader>t', ':bot terminal', {noremap = true, silent = true})
---
+++ nvim/lua/plugins/neoscroll.lua
@@ -0,0 +1,9 @@
+return {
+    "karb94/neoscroll.nvim",
+    opts = {
+	hide_cursor = true,  -- hide cursor while scrolling
+	stop_eof = true,  -- stop at <EOF> when scrolling downwards
+	easing = 'quadratic',
+	performance_mode = true,
+    },
+}
