8e9a0b313f52ea9ea84b38201bbb37b544cf080c
Rsb23 <rsbarbu23@gmail.com>
2025-07-22 20:33:37 -0400

    made terminal cursor line

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,6 @@ vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
+
+vim.o.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:ver25'  -- default except terminal cursor which is now a thin, vertical 25% width, blinking bar
+
