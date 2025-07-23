6661c6b94c0076103b3a0cb35d09424000267a6c
Rsb23 <rsbarbu23@gmail.com>
2025-07-22 23:06:42 -0400

    added quicker terminal and NvimTree keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,18 +1,48 @@
--- folder tree
-vim.keymap.set('n', '<leader>e', function()
-    vim.cmd("NvimTreeFocus")
-end)
-
 -- leader keys
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
--- tab movement
-vim.api.nvim_set_keymap('t','<ESC>','<C-W>C', {noremap = true})  -- easier way to exit terminal mode
-
 -- enable spellcheck (turned off by default)
 vim.o.spell = true
-vim.o.spelllang = { "en_us" }
+vim.o.spelllang = "en_us"
+
+-- folder tree
+vim.keymap.set('n', '<leader>e', function()
+    vim.cmd("NvimTreeToggle")
+end)
 
 -- bottom terminal
-vim.api.nvim_set_keymay('n', '<Leader>t', ':bot terminal', {noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('n', '<Leader>t', ':bot terminal<CR> :resize 15<CR> :setlocal nospell<CR> i', {noremap = true, silent = true})
+
+-- Term Toggle Function
+-- https://www.reddit.com/r/vim/comments/8n5bzs/using_neovim_is_there_a_way_to_display_a_terminal/
+local term_buf = nil
+local term_win = nil
+
+function TermToggle(height)
+    if term_win and vim.api.nvim_win_is_valid(term_win) then
+        vim.cmd("hide")
+    else
+        vim.cmd("bot new")
+        local new_buf = vim.api.nvim_get_current_buf()
+        vim.cmd("resize " .. height)
+        if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+            vim.cmd("buffer " .. term_buf) -- go to terminal buffer
+            vim.cmd("bd " .. new_buf) -- cleanup new buffer
+	    vim.cmd("setlocal nospell")
+	    vim.cmd("normal i")
+        else
+            vim.cmd("terminal")
+            term_buf = vim.api.nvim_get_current_buf()
+            vim.wo.number = false
+            vim.wo.relativenumber = false
+            vim.wo.signcolumn = "no"
+        end
+    vim.cmd("startinsert!")
+    term_win = vim.api.nvim_get_current_win()
+    end
+end
+
+vim.keymap.set('n', '<Leader>t', ':lua TermToggle(15)<CR>', {noremap = true}) 
+vim.api.nvim_set_keymap('t', '<ESC>','<C-\\><C-N><C-W>w<C-W>w', {noremap = true})  -- switch to different window when ESC pressed in terminal
+vim.api.nvim_set_keymap('t', '<C-Q>', '<C-\\><C-N><C-W>c<C-W>w', {noremap = true})  -- completely quit terminal and move to working window when Ctrl + Q is pressed
