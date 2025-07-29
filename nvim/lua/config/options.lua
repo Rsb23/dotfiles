f741f9646db47f72663714e4be8b5ffbd1597f1a
Rsb23 <rsbarbu23@gmail.com>
2025-07-29 12:54:50 -0400

    added autowrite capabilities nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,5 @@ vim.opt.shiftwidth = 4
 
 vim.o.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:ver25'  -- default except terminal cursor which is now a thin, vertical 25% width, blinking bar
 
+vim.o.autowriteall = true  -- automatically write file without having to :w
+vim.o.hidden = false -- by default nvim moves buffers to be hidden when switching to a new buffer preventing autowrite behaviour, this setting makes sure the above autowrite setting works
